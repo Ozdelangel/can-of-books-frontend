f395c7db8fc176ba4d076f0a6a2d256b02fea5cc
Chris Rarig <cwrarig20@gmail.com>
2021-10-26 16:22:26 -0500

    added button logic

--- src/Profile.js
+++ src/Profile.js
@@ -5,7 +5,13 @@ class Profile extends Component {
   render() {
     /* TODO: render information about logged in user */
     /* STRETCH TODO: if no logged in user then redirect home */
-    return <p>Profile page coming soon</p>
+    return( 
+      <>
+    <img src='https://via.placeholder.com/150'alt="Pic"/>
+    <p>name:</p>
+    <p>email:</p>
+    </>
+    )
   }
 };
 
